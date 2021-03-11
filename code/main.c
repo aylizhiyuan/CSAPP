da236c37c6a9f16245f40e04210b84e71c060bfc
zhiyuan Li <zhiyuan.li@thoughtworks.com>
2021-03-11 16:06:58 +0800

    至少是个进步，哈哈....

---
+++ code/main.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-11 12:08:10
+ * @LastEditTime: 2021-03-11 12:12:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /csapp/code/main.c
+ */
+int sum(int *a,int n);
+int array[2] = {1,2};
+int main(){
+    int val = sum(array,2);
+    return val;
+}
